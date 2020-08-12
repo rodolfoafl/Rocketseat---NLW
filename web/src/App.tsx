b42a477d248a69068037fa4f6a3da09433d2b7f7
Rodolfo <rodolfoafl@gmail.com>
2020-08-12 01:54:22 -0300

    Dia 1 - Parte 1

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Landing from "./pages/Landing";
+
+import "./assets/styles/global.css";
+
+const App = () => {
+  return <Landing />;
+};
+
+export default App;
